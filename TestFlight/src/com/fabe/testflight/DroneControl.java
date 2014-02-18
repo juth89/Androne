94f9391eb5a70ce73d4d3f10e51541a992333d50
juth89 <Julian.Thiel@gmx.de>
2014-02-18 10:27:15 +0100

    Project added

---
+++ TestFlight/src/com/fabe/testflight/DroneControl.java
@@ -0,0 +1,69 @@
+package com.fabe.testflight;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+import com.codeminders.ardrone.ARDrone;
+import com.codeminders.ardrone.DroneStatusChangeListener;
+import com.codeminders.ardrone.NavDataListener;
+import com.codeminders.ardrone.ARDrone.VideoChannel;
+import com.codeminders.ardrone.NavData;
+
+
+public class DroneControl implements DroneStatusChangeListener, NavDataListener{
+
+	private static final long CONNECT_TIMEOUT = 2000;
+	private ARDrone drone;
+	
+	public DroneControl() {
+		super();
+	}
+	
+	public void init() throws UnknownHostException {
+		drone = new ARDrone();
+		drone.addStatusChangeListener(this);
+		drone.addNavDataListener(this);
+	}
+	
+	
+	public void connect() throws IOException {
+		drone.connect();
+		drone.waitForReady(CONNECT_TIMEOUT);
+		drone.clearEmergencySignal();
+	}
+	
+	
+	public void takeOff() throws IOException {
+		drone.takeOff();
+	}
+	
+	
+	public void land() throws IOException {
+		drone.land();
+	}
+	
+	
+	public void disconnect() throws IOException {
+		drone.disconnect();
+		drone = null;
+	}
+	
+	
+	@Override
+	public void ready() {
+		try {
+			drone.selectVideoChannel(VideoChannel.HORIZONTAL_ONLY);
+			drone.setCombinedYawMode(true);
+			drone.trim();
+		} catch (IOException e) {
+			drone.changeToErrorState(e);
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void navDataReceived(NavData navData) {
+		System.out.println("Battery status: " + navData.getBattery());
+		System.out.println("Drone is flying: " + navData.isFlying());
+	}
+}
---
+++ TestFlight/src/com/fabe/testflight/FullscreenActivity.java
@@ -0,0 +1,160 @@
+package com.fabe.testflight;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+import com.codeminders.ardrone.ARDrone;
+import com.fabe.testflight.util.SystemUiHider;
+import com.fabe.testflight.R;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+/* @see SystemUiHider */
+public class FullscreenActivity extends Activity 
+{
+	private SystemUiHider mSystemUiHider;
+    private static final long CONNECT_TIMEOUT = 3000;
+    
+	private static final long IN_AIR_TIME = 10000;
+	private static final long DISCONNECT_TIMEOUT = 2000;
+    
+    private DroneControl droneControl;
+	
+    @Override
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_fullscreen);
+
+		//final View controlsView = findViewById(R.id.fullscreen_content_controls);
+		final View contentView = findViewById(R.id.fullscreen_content);
+
+		// Set up the user interaction to manually show or hide the system UI.
+		contentView.setOnClickListener(new View.OnClickListener() 
+		{public void onClick(View view) {}});
+
+			// Upon interacting with UI controls, delay any scheduled hide()
+			// operations to prevent the jarring behavior of controls going away
+			// while interacting with the UI.
+			findViewById(R.id.BTstart_land).setOnTouchListener(mDelayHideTouchListener);
+			findViewById(R.id.BTemergency).setOnTouchListener(mDelayHideTouchListener);
+			findViewById(R.id.BTconnect).setOnTouchListener(mDelayHideTouchListener);
+			findViewById(R.id.BTup).setOnTouchListener(mDelayHideTouchListener);
+			findViewById(R.id.BTdown).setOnTouchListener(mDelayHideTouchListener);
+			findViewById(R.id.BTforward).setOnTouchListener(mDelayHideTouchListener);
+			findViewById(R.id.BTbackward).setOnTouchListener(mDelayHideTouchListener);
+			findViewById(R.id.BTleft).setOnTouchListener(mDelayHideTouchListener);
+			findViewById(R.id.BTright).setOnTouchListener(mDelayHideTouchListener);
+			findViewById(R.id.BTrotate_left).setOnTouchListener(mDelayHideTouchListener);
+			findViewById(R.id.BTrotate_right).setOnTouchListener(mDelayHideTouchListener);
+
+			try {
+				droneControl = new DroneControl();
+				droneControl.init();
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			}
+	}
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) 
+	{
+		super.onPostCreate(savedInstanceState);
+	}
+
+	View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() 
+	{
+		@Override
+		public boolean onTouch(View view, MotionEvent motionEvent) 
+		{
+			return false;
+		}
+	};
+
+	public void onButtonClick (View view) //throws Exception
+    {
+    	Log.v("Testflight", "onButtonClick");
+    	Toast toast = Toast.makeText(FullscreenActivity.this, "EMERCENCY", Toast.LENGTH_LONG);
+    	
+    	// Button Auswahl
+    	switch(view.getId())
+    	{
+	    	case R.id.BTemergency: 	
+	    		toast = Toast.makeText(FullscreenActivity.this, "EMERCENCY", Toast.LENGTH_LONG);
+		    	toast.show();
+	    		break;
+    	
+	    	case R.id.BTconnect:
+	    		toast = Toast.makeText(FullscreenActivity.this, "CONNECT", Toast.LENGTH_LONG);
+		    	toast.show();
+		    	try {
+		    		droneControl.connect();
+		    		droneControl.takeOff();
+		    		Thread.sleep(IN_AIR_TIME);
+		    		droneControl.land();
+		    		Thread.sleep(DISCONNECT_TIMEOUT);
+		    		droneControl.disconnect();
+		    	} catch(Exception e) {
+		    		
+		    	}
+	    		break;
+	    		
+	    	case R.id.BTstart_land:
+	    		toast = Toast.makeText(FullscreenActivity.this, "START_LAND", Toast.LENGTH_LONG);
+		    	toast.show();
+	    		break;
+	    		
+	    	case R.id.BTup:
+	    		toast = Toast.makeText(FullscreenActivity.this, "UP", Toast.LENGTH_LONG);
+		    	toast.show();
+	    		break;
+	    		
+	    	case R.id.BTdown:
+	    		toast = Toast.makeText(FullscreenActivity.this, "DOWN", Toast.LENGTH_LONG);
+		    	toast.show();
+	    		break;
+	    		
+	    	case R.id.BTforward:
+	    		toast = Toast.makeText(FullscreenActivity.this, "FORWARD", Toast.LENGTH_LONG);
+		    	toast.show();
+	    		break;
+	    		
+	    	case R.id.BTbackward:
+	    		toast = Toast.makeText(FullscreenActivity.this, "BACKWARD", Toast.LENGTH_LONG);
+		    	toast.show();
+	    		break;
+	    		
+	    	case R.id.BTleft: 
+	    		toast = Toast.makeText(FullscreenActivity.this, "LEFT", Toast.LENGTH_LONG);
+		    	toast.show();
+	    		break;
+	    		
+	    	case R.id.BTright:
+	    		toast = Toast.makeText(FullscreenActivity.this, "RIGHT", Toast.LENGTH_LONG);
+		    	toast.show();
+	    		break;
+	    		
+	    	case R.id.BTrotate_left:
+	    		toast = Toast.makeText(FullscreenActivity.this, "ROTATE_LEFT", Toast.LENGTH_LONG);
+		    	toast.show();
+	    		break;
+	    		
+	    	case R.id.BTrotate_right: 		
+	    		toast = Toast.makeText(FullscreenActivity.this, "ROTATE_RIGHT", Toast.LENGTH_LONG);
+		    	toast.show();
+	    		break;
+	    		
+	    	default:
+	    		break;
+    	}
+    }
+}
