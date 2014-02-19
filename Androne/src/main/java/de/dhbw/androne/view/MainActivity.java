66fbaf79f5194547661148f5594f8667f044e712
juth89 <Julian.Thiel@gmx.de>
2014-02-19 22:42:53 +0100

    OptionsItem - Connect added

--- Androne/src/main/java/de/dhbw/androne/view/MainActivity.java
+++ Androne/src/main/java/de/dhbw/androne/view/MainActivity.java
@@ -7,6 +7,8 @@ import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MenuItem.OnMenuItemClickListener;
 import de.dhbw.androne.view.adapter.TabsPagerAdapter;
 import de.dhbw.androne.view.viewpager.NonSwipeableViewPager;
 
@@ -35,6 +37,19 @@ public class MainActivity extends FragmentActivity implements TabListener {
 		for(String tabName : tabNames) {
 			actionBar.addTab(actionBar.newTab().setText(tabName).setTabListener(this));
 		}
+//		MenuItem item = (MenuItem) findViewById(R.id.action_connect);
+//		item.setOnMenuItemClickListener(new OnMenuItemClickListener() {
+//			
+//			@Override
+//			public boolean onMenuItemClick(MenuItem item) {
+//				if(item.getTitle().equals("Connect")) {
+//					item.setTitle("Disconnect");
+//				} else {
+//					item.setTitle("Connect");
+//				}
+//				return false;
+//			}
+//		});
 	}
 
 	
@@ -42,6 +57,29 @@ public class MainActivity extends FragmentActivity implements TabListener {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch(item.getItemId()){
+		case R.id.action_connect:
+
+			if(item.getTitle().equals("Connect")) {
+				item.setTitle(R.string.action_disconnect);
+			} else {
+				item.setTitle(R.string.action_connect);
+			}
+			
+			return false;
+		case R.id.action_settings:
+			
+			return false;
+		default:
+			super.onOptionsItemSelected(item);
+		}
+		
+        return super.onOptionsItemSelected(item);
 	}
 
 	
